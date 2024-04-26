c5f3d891543107979f181bc046306e598d1ca0c5
andy7076 <auto7076@gmail.com>
2024-04-26 17:23:43 +0800

    添加备案

--- components/CustomFooter.tsx
+++ components/CustomFooter.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
 export default function PluginUI(props: any) {
-  return null;
+  return <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0 0 24px' }}>
+    <a style={{ fontSize: 12, }} href='https://beian.miit.gov.cn' target='_blank'>浙ICP备18013294号-1</a>
+  </div>;
 }
