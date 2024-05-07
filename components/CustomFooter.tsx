5fab334d57c185f162e720fb52d70e968acca74b
andy7076 <wyz@yyfollower.com>
2024-05-07 14:24:35 +0800

    修改备案号

--- components/CustomFooter.tsx
+++ components/CustomFooter.tsx
@@ -1,5 +1,5 @@
 export default function PluginUI(props: any) {
   return <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', padding: '0 0 24px' }}>
-    <a style={{ fontSize: 12, }} href='https://beian.miit.gov.cn' target='_blank'>浙ICP备18013294号-1</a>
+    <a style={{ fontSize: 12, }} href='https://beian.miit.gov.cn' target='_blank'>浙ICP备2023004975号-2</a>
   </div>;
 }
